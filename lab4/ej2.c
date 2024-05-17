b59ecf307592fd62a4e4d7275dc41e9f93166ffa
Davidson993 <davidson4622@gmail.com>
2024-05-17 09:35:56 -0600

    modificado  de subir el ejercicio 1

--- lab4/ej2.c
+++ lab4/ej2.c
@@ -4,22 +4,52 @@
 int encontrarMinimo(int arreglo[], int longitud) {
     int minimo = arreglo[0]; // Inicializamos el mínimo con el primer elemento del arreglo
     
-    // Iteramos sobre el arreglo para encontrar el mínimo
+                                                        //hacemos iteraciones sobre el arreglo para buscar el mínimo
     for (int i = 1; i < longitud; i++) {
         if (arreglo[i] < minimo) {
             minimo = arreglo[i];
         }
     }
     
-    return minimo; // Retornamos el valor mínimo encontrado
+    return minimo;                                      // Retornamos el valor mínimo encontrado
 }
+
+
+int encontrarMaximo(int arreglo[], int longitud) {
+    int maximo = arreglo[0];                           // Inicializamos el máximo con el primer elemento del arreglo
+    for (int i = 1; i < longitud; i++) {
+        if (arreglo[i] > maximo) {
+            maximo = arreglo[i];
+        }
+    }
+    return maximo;
+}  
+
+
+void encontrarMinMax(int *arreglo, int * minimo, int * maximo, int longitud){
+*minimo = *maximo = arreglo[0];                         //inicializamos los valores de los punteros
+    for (int i = 1; i < longitud; i++) {
+        if (*(arreglo + i) < *minimo) {
+            *minimo = *(arreglo + i);
+        } else if (*(arreglo + i) > *maximo) {
+            *maximo = *(arreglo + i);
+        }
+    }
+}   
+
+
 
 int main() {
     int arreglo[] = {21, 24, 65, 3, 56, 12, 35, 15};
     int longitud = sizeof(arreglo) / sizeof(arreglo[0]); // Calculamos la longitud del arreglo
     
-    // Llamamos a la función para encontrar el mínimo
+                                                     // Llamamos a la función para encontrar el mínimo
     int minimo = encontrarMinimo(arreglo, longitud);
-    
-    // Imprimimos el mínimo encontrado
+    int maximo = encontrarMaximo(arreglo, longitud);
+    void encontrarMinMax(int *arreglo, int * minimo, int * maximo, int longitud);
+   
+                                                     // Imprimimos el mínimo encontrado
     printf("El valor minimo en el arreglo es: %d\n", minimo);
+    printf("El valor máximo en el arreglo es: %d\n", maximo);
+    return 0;
+}
